2aacab1c169ffc8eaad705cd01888ab332b1d8cf
Satsko <pingvinik1029@gmail.com>
2024-03-05 00:32:13 +0300

    add MY_COW

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -2,23 +2,33 @@ import argparse
 import urllib.request
 import os
 import random
-from cowsay import cowsay, list_cows
+from cowsay import cowsay, list_cows, read_dot_cow
+from io import StringIO
+
+MY_COW = read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+  $thoughts
+   $thoughts
+      ^(;,;)^
+
+EOC
+"""))
 
 def ask(prompt: str, valid: list[str] = None):
     cow_print = random.randrange(0, len(list_cows()))
-    print(cowsay(prompt, cow=list_cows()[cow_print]))
+    print(cowsay(prompt, cowfile=MY_COW))
     inp_word = input()
     if valid is not None:
         while inp_word not in valid:
             if inp_word == ".": return inp_word
-            print(cowsay(prompt, cow=list_cows()[cow_print]))
+            print(cowsay(prompt, cowfile=MY_COW))
             inp_word = input()
     return inp_word
 
 def inform(format_string: str, bulls: int, cows: int):
     #print(format_string.format(bulls, cows))
     cow_print = random.randrange(0, len(list_cows()))
-    print(cowsay(format_string.format(bulls, cows), cow=list_cows()[cow_print]))
+    print(cowsay(format_string.format(bulls, cows), cowfile=MY_COW))
     
 def bullscows(guess: str, secret: str):
     bulls = 0
