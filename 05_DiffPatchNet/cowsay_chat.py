20d9a5e6ba813b52f9c4ecb830921da17720ed29
Satsko <pingvinik1029@gmail.com>
2024-03-19 00:01:16 +0300

    Add say and yield

--- 05_DiffPatchNet/cowsay_chat.py
+++ 05_DiffPatchNet/cowsay_chat.py
@@ -26,7 +26,7 @@ async def chat(reader, writer):
                     clients[me] = asyncio.Queue()
                     cowlst.remove(message[1])
                     reg = True
-                    writer.write("Successfully registration:\n".encode())
+                    writer.write("Successfull registration:\n".encode())
                     await writer.drain()
                     receive.cancel()
                     receive = asyncio.create_task(clients[me].get())
@@ -43,11 +43,33 @@ async def chat(reader, writer):
                   await writer.wait_closed()
                   return
                 elif (message[0] == 'cows'):
-                    writer.write(f"Available usernames (cows): {', '.join(cows_list)}\n".encode())
+                    writer.write(f"Available usernames (cows): {', '.join(cowlst)}\n".encode())
                     await writer.drain()
                 elif (message[0] == 'who'):
                     writer.write(f"Registered users: {', '.join(clients.keys())}\n".encode())
                     await writer.drain()
+                elif (message[0] == "say"):
+                    if not reg:
+                        writer.write("You need register to send messages\n".encode())
+                        await writer.drain()
+                        continue
+                    if (message[1] in clients.keys()):
+                        await clients[message[1]].put(f"Message from {me}:\n {cowsay.cowsay((' '.join(message[2:])).strip(), cow=me)}")
+                        writer.write("Message is sent\n".encode())
+                        await writer.drain()
+                  else:
+                    writer.write("There no user with this name\n".encode())
+                    await writer.drain()
+                elif (message[0] == 'yield'):
+                  if not reg:
+                    writer.write("You need register to send messages\n".encode())
+                    await writer.drain()
+                    continue
+                  for dst in clients.values():
+                    if dst is not clients[me]:
+                      await dst.put(f"Message from {me}:\n {cowsay(' '.join(message[1:]).strip(), cow=me)}")
+                  writer.write("Message is sent \n".encode())
+                  await writer.drain()
                 else:
                   writer.write("Wrong command!\n".encode())
                   await writer.drain()
